84fbffd9821ee43ac36a3f513cebc275bb727897
corneliusyan <onel.raja@gmail.com>
2018-09-14 10:53:30 +0700

    tambah main

--- board.py
+++ board.py
@@ -1,7 +1,15 @@
 # Chessboard containing 
 
+file_input = 'input.txt'
 
 class Board:
 	def __init__(self):
-		return
+		with open(file_input, 'rt') as f_in:
+			current_list = []
+			lines = f_in.readlines()
+			for line in lines:
+				clear = line.replace('\n','')
+				current_list.append(line.split())
+		
+		return self
 
--- main.py
+++ main.py
@@ -1,34 +1,30 @@
 # FIRST project
 # N-ything problem, a modified version of the famous N-queens Problem in AI
 
-import hill
+import Hill
 import genetic
 import annealing
-import board as b
+import board
 
 
 file_input = "input.txt"
 piecelist = []
 
-def readFile():
-	with open(file_input, 'rt') as f_in:
-		current_list = []
-		lines = f_in.readlines()
-		for line in lines:
-			clear = line.replace('\n','')
-			current_list.append(line.split())
-	return current_list
-	
+def init():
+
+	return board.Board()
 
 if __name__ == "__main__":
-	piecelist = readFile()
-	mboard = b.Board()
+
+	mboard = init()
+	print(mboard)
+
 	for i in range(0,8):
 		for j in range(0,8):
-			print(mboard.ChessBoard[i][j],end=' ')
+			print(".",end = ' ')
 		print()
 
-	hill.solve_hill(mboard)
+	Hill.solve_hill(mboard)
 	annealing.solve_annealing(mboard)
 	genetic.solve_genetic(mboard)
 
